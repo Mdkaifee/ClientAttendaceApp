86e0528734fad5a8ef075945f496802fb749fe36
Mdkaifee <kaifeeeminenence@gmail.com>
2026-03-31 17:32:40 +0530

    Updated gradleproperties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val uploadStoreFile =
+    (project.findProperty("MYAPP_UPLOAD_STORE_FILE") as String?)
+        ?.takeIf { it.isNotBlank() }
+        ?.let { rootProject.file(it) }
+
 android {
     namespace = "com.example.attendance_client"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+     compileSdk = flutter.compileSdkVersion
+     ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -29,16 +34,16 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = project.findProperty("MYAPP_UPLOAD_KEY_ALIAS") as String
-            keyPassword = project.findProperty("MYAPP_UPLOAD_KEY_PASSWORD") as String
-            storeFile = file(project.findProperty("MYAPP_UPLOAD_STORE_FILE") as String)
-            storePassword = project.findProperty("MYAPP_UPLOAD_STORE_PASSWORD") as String
+            keyAlias = project.findProperty("MYAPP_UPLOAD_KEY_ALIAS") as String?
+            keyPassword = project.findProperty("MYAPP_UPLOAD_KEY_PASSWORD") as String?
+            storeFile = uploadStoreFile
+            storePassword = project.findProperty("MYAPP_UPLOAD_STORE_PASSWORD") as String?
         }
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release") // ✅ FIXED
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
